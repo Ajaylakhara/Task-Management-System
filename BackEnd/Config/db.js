8ab4d2b4a3151608bf81db318c859c11f0e047e4
Ajay Lakhara <88051262+Ajaylakhara@users.noreply.github.com>
2025-08-06 11:06:12 +0530

    Delete BackEnd directory

--- BackEnd/Config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-
-export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://TaskManagement:Ajay@taskcluster0.6q72axx.mongodb.net/Taskflow').then(()=>console.log("DB Connected"));
-}
